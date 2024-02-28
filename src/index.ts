e808b34dd9313180a2b81c01e079132a7f3ae4cb
erdfern <rexsomnia@pm.me>
2024-02-29 00:16:19 +0100

    fix: import katex css

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import "katex/dist/katex.min.css"
+
 export { default as VueKatexPlugin, pluginOptionsInjectionKey } from "./plugin";
 export { default as MathRenderer } from "./components/MathRenderer.vue";
 export type { VueKatexOptions, KatexDirective, KatexDirectiveValue } from "./types";
